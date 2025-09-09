f9f1effb8f181deb7f12d16ae4b2fa7e4ff0971e
CollinsKobby <collinsnukporti888@gmail.com>
2025-09-09 06:08:22 +0000

    App config

--- miniproject/freelance_marketplace/GigGh/apps.py
+++ miniproject/freelance_marketplace/GigGh/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GigghConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GigGh'
+    name = 'freelance_marketplace.GigGh'
