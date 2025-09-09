023b688c13390068ce343e608390375a67afd247
CollinsKobby <collinsnukporti888@gmail.com>
2025-09-09 06:00:51 +0000

    settings

--- miniproject/freelance_marketplace/freelance_marketplace/settings.py
+++ miniproject/freelance_marketplace/freelance_marketplace/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'GigGh.apps.GigghConfig',
+    'freelance_marketplace.GigGh.apps.GigghConfig',
     'channels',
 ]
 
